8f4535abec268d47191a9dc8099096f8e870abe6
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-16 19:02:19 +0000

    Test.1 check oyster card hold amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,9 @@ require "oystercard.rb"
 
 describe Oystercard do
 
+  it 'Test that card holds an amount of money' do
+    Oystercard = Card.new(50)
+    expect{ subject(50) }.to eq (50)
+  end
+
 end
