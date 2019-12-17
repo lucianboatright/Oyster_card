2dee3ea8d11dc4436994a8944df5039d6da17d72
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-17 14:50:45 +0000

    Passed stated saved

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,22 +1,34 @@
 class Oystercard
+
   DEAFULT_LIMIT = 90
 
   attr_reader :card_balance
 
-  def initialize(limit=DEFAULT_LIMIT)
-    @limit = limit
+  def initialize
     @card_balance = 0
   end
 
   def top_up(amount)
-    @card_balance += limit 
+    fail 'You have reached the limit of £90' if amount + @card_balance > DEAFULT_LIMIT
+    @card_balance += amount
   end
 
-  def limit(limit)
-    if @card_balance + top_up > @limit
-      raise 'You have reached the limit of £90'
-    else
-      topup(amount)
-    end
-  end
 end
+
+
+
+
+# class Oystercard
+#   DEAFULT_LIMIT = 90
+
+#   attr_reader :card_balance
+
+#   def initialize
+#     @card_balance = 0
+#   end
+
+#   def top_up(amount)
+#     fail 'You have reached the limit of £90' if amount + @balance > DEAFULT_LIMIT
+#     @card_balance += amount 
+#   end
+# end
